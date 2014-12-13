7f31ca2913a07b802fc49690ac64512e195de321
magdapolieri <magdalena.polieri@gmail.com>
2014-12-13 11:40:34 +0100

    modif par ci par la

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-	$('.fade').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  slide: 'div',
-  cssEase: 'linear'
+	$('.slider').slick();
 });
-
-
